2d685501889f450ff6a3b9413aec27d0057aba72
CipherKode <kodecipher@gmail.com>
2025-08-17 03:58:28 +0530

    Snehal | Dropdown buttons fixed

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -7,7 +7,7 @@ import React from "react";
 import { User, Shield, Settings } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { DataTable_Search } from "@/src/components/ui/data-table-search";
-import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuItem } from "@radix-ui/react-dropdown-menu";
+import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuItem } from "@/components/ui/dropdown-menu";
 
 import Image from "next/image";
 
@@ -56,7 +56,7 @@ const columns = [
     cell: ({ row }: any) => (
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
-          <Button variant="ghost" size="icon">
+          <Button variant="ghost" size="icon" className="w-8 h-8">
             <Settings className="w-5 h-5" />
           </Button>
         </DropdownMenuTrigger>
