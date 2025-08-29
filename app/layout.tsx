d80544e8d87836364d7f3c8309767c40a33b2074
CipherKode <kodecipher@gmail.com>
2025-08-30 01:22:12 +0530

    Snehal | Extracted Sidiebar compnent in another file

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,23 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import React from "react";
-import {
-  Sidebar,
-  SidebarContent,
-  SidebarFooter,
-  SidebarGroup,
-  SidebarHeader,
-  SidebarProvider,
-  SidebarGroupLabel,
-  SidebarGroupContent,
-  SidebarMenuButton,
-  SidebarMenu,
-  SidebarMenuItem,
-} from "@/components/ui/sidebar"; // shadcn sidebar import
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { SIDEBAR_LINKS } from "@/assets/constants/sidebar-links";
 import { ThemeProvider } from "@/components/ui/theme-provider";
-import { FileText } from "lucide-react";
+import AppSidebar from "@/components/layout/Sidebar"; // added
 
 const urbanist = Inter({
   variable: "--font-urbanist-sans",
@@ -46,76 +31,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <div className="min-h-[100vh] flex">
-            <div className="sidebar-wrapper">
-              <SidebarProvider>
-                <Sidebar className="w-64 ">
-                  <SidebarHeader className="p-[14px] bg-yellow-500 px-6">
-                    <span className="font-bold text-md">IITian Squad</span>
-                  </SidebarHeader>
-
-                  <SidebarContent className="p-2">
-                    <SidebarGroup>
-                      <SidebarGroupLabel>Admin management</SidebarGroupLabel>
-                      <SidebarGroupContent>
-                        <SidebarMenu>
-                          {SIDEBAR_LINKS.adminManagement.map((item) => (
-                            <SidebarMenuItem key={item.title}>
-                              <SidebarMenuButton asChild>
-                                <a href={item.url}>
-                                  <item.icon />
-                                  <span>{item.title}</span>
-                                </a>
-                              </SidebarMenuButton>
-                            </SidebarMenuItem>
-                          ))}
-                        </SidebarMenu>
-                      </SidebarGroupContent>
-                    </SidebarGroup>
-                    <SidebarGroup>
-                      <SidebarGroupLabel>Content management</SidebarGroupLabel>
-                      <SidebarGroupContent className="space-y-2">
-                        <SidebarMenu>
-                          {SIDEBAR_LINKS.contentManagement.map((item) => (
-                            <SidebarMenuItem key={item.title}>
-                              <SidebarMenuButton asChild>
-                                <a href={item.url}>
-                                  <item.icon />
-                                  <span>{item.title}</span>
-                                </a>
-                              </SidebarMenuButton>
-                            </SidebarMenuItem>
-                          ))}
-                        </SidebarMenu>
-                        <SidebarMenu>
-                          <SidebarMenuItem>
-                            <SidebarMenuButton asChild>
-                              <a href="/blog-management">
-                                <FileText />
-                                <span>Blog Management</span>
-                              </a>
-                            </SidebarMenuButton>
-                          </SidebarMenuItem>
-                        </SidebarMenu>
-                      </SidebarGroupContent>
-                    </SidebarGroup>
-                  </SidebarContent>
-                  <SidebarFooter>
-                    <div className="flex items-start gap-4 px-4 py-4">
-                      <Avatar>
-                        <AvatarImage src="/profile.jpg" alt="Profile" />
-                        <AvatarFallback>IS</AvatarFallback>
-                      </Avatar>
-                      <div>
-                        <div className="font-medium">Your Name</div>
-                        <div className="text-xs text-muted-foreground">
-                          Admin
-                        </div>
-                      </div>
-                    </div>
-                  </SidebarFooter>
-                </Sidebar>
-              </SidebarProvider>
-            </div>
+            <AppSidebar />
             <main className=" grow-1 h-full shrink-0">{children}</main>
           </div>
         </ThemeProvider>
